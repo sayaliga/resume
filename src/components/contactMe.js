4f6fe664dc738028861d8c0d0ee9ef1578f48642
sayaliga <sayali.gaikwad1994@gmail.com>
2018-01-28 10:04:52 +0530

    Misc changes in contact me section

--- src/components/contactMe.js
+++ src/components/contactMe.js
@@ -15,31 +15,36 @@ class Form extends React.Component {
             <section className="contactme">
                 <div className="row">
                     <div className="col-sm-6">
-                        <p>
-                            Drop me an email and I'll get back to you.
-                        </p>
-                        <form className="well" action="https://formspree.io/sayali.gaikwad1994@gmail.com" method="POST">
-                            <div className="form-group">
-                                <input className="form-control" name="email" type="email" placeholder="Enter your email" required></input>
-                            </div>
-                            <div className="form-group">
-                                <textarea className="form-control" name="message" placeholder="Your message" required></textarea>
-                                <input type="hidden" name="_next" value="https://sayaliga.github.io/resume/thankyou.html"></input>
-                            </div>
-                            <div className="form-group">
-                                <button onClick={this.sendEmail}>
-                                    <i className="fa fa-send text-center" aria-hidden="true">&nbsp;&nbsp;Send</i>
-                                </button>
-                                <button type="reset">
-                                    <i className="fa fa-close text-center" aria-hidden="true">&nbsp;&nbsp;Reset</i>
-                                </button>
-                            </div>
+                        <form id="gform" className="well" action="https://script.google.com/macros/s/AKfycbzqeHjlQ4wMHRF_UIxqarLnuEEu4Il2nAUDAhLz0OnlIUCd9rD6/exec" method="POST" data-email="sayali.gaikwad1994@gmail.com">
+                            <p>
+                                Drop me an email and I'll get back to you.
+                            </p>
+                            <fieldset className="form-group">
+                                <label htmlFor="name">Name: </label>
+                                <input className="form-control" id="name" name="name" placeholder="Your name" />
+                            </fieldset>
+
+                            <fieldset className="form-group">
+                                <label htmlFor="name">E-mail: </label>
+                                <input className="form-control" id="email" name="email" type="email" placeholder="Your E-Mail" required/>
+                            </fieldset>
+                            
+                            <fieldset className="form-group">
+                                <label htmlFor="message">Message: </label>
+                                <textarea className="form-control" id="message" name="message" rows="10"
+                                placeholder="Leave your message here.."></textarea>
+                            </fieldset>
+                            <button>Send</button>
                         </form>
-                        <p>
-                            Or connect with me on linked in - <SocialIcons />
-                        </p>
                     </div>
                 </div>
+                <div style={{display: 'none'}} id="thankyou_message">
+                    <h2><em>Thanks</em> for contacting!
+                    I will get back to you soon!</h2>
+                </div>
+                <p>
+                    Connect with me on linked in - <SocialIcons />
+                </p>
             </section>
         );
     }
